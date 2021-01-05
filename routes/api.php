dd414ef4e929fcaebc8deeb60aacc781cdd0ca83
cristian <cristianjojoa01@gmail.com>
2021-01-04 21:55:00 -0500

    ajustes

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return [
+        'result' => true,
+    ];
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Get list of meetings.
+Route::get('/meetings', 'Zoom\MeetingController@list');
+
+// Create meeting room using topic, agenda, start_time.
+Route::post('/meetings', 'Zoom\MeetingController@create');
+
+// Get information of the meeting room by ID.
+Route::get('/meetings/{id}', 'Zoom\MeetingController@get')->where('id', '[0-9]+');
+Route::patch('/meetings/{id}', 'Zoom\MeetingController@update')->where('id', '[0-9]+');
+Route::delete('/meetings/{id}', 'Zoom\MeetingController@delete')->where('id', '[0-9]+');
+
+
+Route::post('/fair/create', 'FairController@create');
+Route::post('/pavilion/create', 'PavilionController@create');
