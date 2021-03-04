98fedef2a90514525971d25288c0bd2b24192329
David Camacho <davithc01@gmail.com>
2021-03-03 22:51:00 -0500

    Update FairSeeder.php

--- database/seeders/FairSeeder.php
+++ database/seeders/FairSeeder.php
@@ -18,8 +18,8 @@ class FairSeeder extends Seeder
 		$fair->name = 'FeriaGanadera2021';
         $fair->description = 'XI Feria Ganadera 2021';
 		$fair->halls_number = 1;
-		$fair->init_date = '2020-12-24';
-		$fair->end_date = '2020-12-31';
+		$fair->init_date = '2021-12-24';
+		$fair->end_date = '2021-12-31';
 		$fair->resources = '{}';
 		$fair->location = '{}';
 		$fair->social_media = '{}';
