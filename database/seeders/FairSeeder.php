03936deb07c2c3ee9c77ad354d8f02cb97982693
David Camacho <davithc01@gmail.com>
2021-03-03 22:33:28 -0500

    seeder feria

---
+++ database/seeders/FairSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Fair;
+use Illuminate\Database\Seeder;
+
+class FairSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fair = new Fair();
+		$fair->name = 'FeriaGanadera2021';
+        $fair->description = 'XI Feria Ganadera 2021';
+		$fair->halls_number = 1;
+		$fair->init_date = '2020-12-24';
+		$fair->end_date = '2020-12-31';
+		$fair->resources = '{}';
+		$fair->location = '{}';
+		$fair->social_media = '{}';
+        $fair->save();
+    }
+}
