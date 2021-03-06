d229db743a8f620c3d47c65b0b773ad1eed1c02c
David Camacho <davithc01@gmail.com>
2021-03-05 23:30:57 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use File;
 
 class UserController extends Controller
 {
@@ -23,6 +25,7 @@ class UserController extends Controller
             'password'=>'required',
             'role_id'=>'required',
             'fair_id'=>'required',
+
         ]);
 
         if ($validator->fails()) {
@@ -34,11 +37,28 @@ class UserController extends Controller
 
         $data = $validator->validated();
         $fileName = null;
-        if (request()->hasFile('image')) {
-            $image_name = date('mdYHis') . uniqid() . $request->file('image')->getClientOriginalName();
-            $path = base_path() . '/public/images_users';
-            $request->file('image')->move($path,$image_name);
-            $fileName = $path.$image_name;
+        //if (request()->hasFile('image')) {
+            //dd(request()->hasFile('image'));
+            //$image_name = date('mdYHis') . uniqid() . $request->file('image')->getClientOriginalName();
+            //$path = base_path() . '/public/images_users';
+            //$request->file('image')->move($path,$image_name);
+            //$fileName = $path.$image_name;
+        //}
+        $fileName = null;
+        if(isset($data['image'])){
+            $image = $request->image;  // your base64 encoded
+            $extension = explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];   // .jpg .png .pdf
+            $path = 'images_users';
+            $fileName = $path . '/' . date('mdYHis') . uniqid() . '_user_' . $user->id .'.' .$extension;
+            if(!Storage::exists($path)){
+                Storage::makeDirectory($path);
+            }
+            
+            $image = str_replace('data:image/png;base64,', '', $image);
+            $image = str_replace('data:image/jpeg;base64,', '', $image);
+            $path = str_replace('\\\\', '/' , base_path());
+            File::put($path. '/public/' . $fileName, base64_decode($image));
+            $fileName = 'http://127.0.0.1:8000/' . $fileName;
         }
 
         $user = new User();
@@ -48,8 +68,8 @@ class UserController extends Controller
         $user->email = $data['email'];
         $user->url_image = $fileName;
         if(isset($data['contact'])){ 
-		  $user->contact = $data['contact'];
-		}
+          $user->contact = $data['contact'];
+        }
         $user->password = Hash::make($data['password']);
         $user->save();
 
@@ -60,12 +80,11 @@ class UserController extends Controller
         $user_rol_fair->save();
 
         if($data['role_id'] == 6){
-            
             $speaker = new Speaker();
             $speaker->user_id = $user->id;
-            $speaker->name = $user->name.' '.$user->last_name;
-            $speaker->description = '';
-            $speaker->resources = '';
+            $speaker->description = $request->speaker["description"];
+            $speaker->title = $request->speaker["title"];
+            $speaker->resources = $request->speaker["resources"];
             $speaker->save();
         }
 
@@ -86,12 +105,13 @@ class UserController extends Controller
 
     public function update(Request $request){
 
-		$validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'user_name'=>'',
             'name'=>'',
             'last_name'=>'',
             'email'=>'email|unique:users,email',
             'password'=>'',
+            'image'=>'',
         ]);
 
         if ($validator->fails()) {
@@ -101,18 +121,30 @@ class UserController extends Controller
             ];
         }
 
-        $data = $validator->validated();
-		
-        $fileName = null;
-        if (request()->hasFile('image')) {
-            $image_name = date('mdYHis') . uniqid() . $request->file('image')->getClientOriginalName();
-            $path = base_path() . '/public/images_users';
-            $request->file('image')->move($path,$image_name);
-            $fileName = $path.$image_name;
-        }
 
         $user = auth()->guard('api')->user();
-
+        $data = $validator->validated();
+        
+        $fileName = null;
+        if(isset($data['image'])){
+            $image = $request->image;  // your base64 encoded
+            $extension = explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];   // .jpg .png .pdf
+            $path = 'images_users';
+            $fileName = $path . '/' . date('mdYHis') . uniqid() . '_user_' . $user->id .'.' .$extension;
+            if(!Storage::exists($path)){
+                Storage::makeDirectory($path);
+            }
+            
+            $image = str_replace('data:image/png;base64,', '', $image);
+            $image = str_replace('data:image/jpeg;base64,', '', $image);
+            $path = str_replace('\\\\', '/' , base_path());
+            File::put($path. '/public/' . $fileName, base64_decode($image));
+            $fileName = 'http://127.0.0.1:8000/' . $fileName;
+        }
+        else {
+           $fileName = $user->url_image;
+        }
+        
         if(isset($data['user_name']))  $user->user_name = $data['user_name'];
         if(isset($data['name'])) $user->name = $data['name'];
         if(isset($data['last_name'])) $user->last_name = $data['last_name'];
@@ -122,7 +154,7 @@ class UserController extends Controller
             $user->contact = $data['contact'];
         }
         if(isset($data['password'])) $user->password = Hash::make($data['password']);
-
+        
         $user->save();
 
         return [
