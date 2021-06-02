8ab2391722d7c242de81dede2e7161f2eb6e39a6
cristian <cristianjojoa01@gmail.com>
2021-06-01 20:40:48 -0500

    ajuste forzar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -26,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(config('app.env') === 'production'){
+            URL::forceSchema('https');
+        }
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\URL;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -27,9 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
-        if(config('app.env') === 'production'){
-            URL::forceSchema('https');
-        }
         ///Passport::personalAccessClientSecret(config('CLIENT_SECRET'));
         //Passport::personalAccessClientId(config('ID'));
         //
