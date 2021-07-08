68c010635cbf5fd5e4d039d574a599302639c252
David Camacho <davithc01@gmail.com>
2021-07-07 22:42:41 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          //\App\Models\User::factory(10)->create();
+		$this->call(FairSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
 		$this->call(StandTypesSeeder::class);
-		$this->call(FairSeeder::class);
 		$this->call(CategorySeeder::class);
 		$this->call(MerchantSeeder::class);
 		$this->call(SpeakerSeeder::class);
