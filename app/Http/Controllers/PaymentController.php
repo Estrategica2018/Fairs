13765a7a43e0288542467fdcaf3d6823d24de5a0
David Camacho <davithc01@gmail.com>
2021-08-20 15:00:41 -0500

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Payment;
+use Illuminate\Support\Str;
 
 class PaymentController extends Controller
 {
@@ -30,13 +31,22 @@ class PaymentController extends Controller
         ]);
 
         $user = auth()->guard('api')->user();
-
-        if ($user) {
+        if($user) {
+            $payment = Payment::where([['user_id',$user->id],['type_order',$request->type],['code_item_order',$request->id],['payment_status',3]])->first();
+            if(!$payment) {
+                $payment = new Payment();
+                $payment->user_id = $user->id;
+                $payment->type_order = $request->type;
+                $payment->code_item_order = $request->id;
+                $payment->reference = dechex(time()). '-' . $user->id  .'-'. Str::random(2);
+                $payment->payment_status = 1;
+                $payment->save();
+            }
+			
           return response()->json([
             'success' => 201, 
             'publicKey'=>'pub_test_EbunIjUmrCtIyrh28fFqr9sFUVqI43XA',
-            //'reference'=>'4XMPGKWWPKWQ',
-			'reference'=>dechex(time()),
+            'reference'=> $payment->reference,
             'currency'=>'COP',
             'message' => 'Referencia de pago tipo ['.$request->type.'] codigo de producto ['.$request->id.']!'
           ]); 
@@ -56,7 +66,7 @@ class PaymentController extends Controller
         $user = auth()->guard('api')->user();
         
         if ($user) {
-          $payment = Payment::where(['user_id'=>$user->id,'type_order'=>$request->type,'code_item_order'=>$request->id])->first();
+          $payment = Payment::where(['user_id'=>$user->id,'type_order'=>$request->type,'code_item_order'=>$request->id,'payment_status'=>3])->first();
           if($payment){
             return response()->json([
             'success' => 201, 
