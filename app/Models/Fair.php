df3613d840b15130b2f7f51c1159143b4b69913f
David Camacho <davithc01@gmail.com>
2022-03-24 23:01:17 -0500

    Update Fair.php

--- app/Models/Fair.php
+++ app/Models/Fair.php
@@ -13,4 +13,10 @@ class Fair extends Model
 
         return $this->hasMany(Pavilion::class,'fair_id','id');
     }
+	
+    public function role_user_fairs (){
+
+        return $this->belongsToMany(Role::class,'role_user_fairs','fair_id','role_id');
+
+    }
 }
