0f5a98c522d410e6757c5f4bdf6706c9eac52dc8
David Camacho <davithc01@gmail.com>
2022-06-03 07:28:19 -0500

    Update successfullPayment.blade.php

--- resources/views/notifications/successfullPayment.blade.php
+++ resources/views/notifications/successfullPayment.blade.php
@@ -75,7 +75,7 @@
     <tr>
         <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                <tr>
+                <tr> 
                     <td bgcolor="#3d98d1" align="center" valign="top" style="border-radius: 4px 4px 0px 0px; color: #00338d; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
                         <h1 style="font-size: 21px; font-weight: 400; margin: 20px; color: white;">¡Confirmación de pago!</h1> 
                     </td>
