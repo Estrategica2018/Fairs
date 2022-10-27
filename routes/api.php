04d162138bce883bbb42368c0465e2cd0c01ca61
David Camacho <davithc01@gmail.com>
2022-10-27 10:08:04 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::delete('/meetings/{id}', 'Zoom\MeetingController@delete')->where('id', '[
 
 //
 Route::post('/fair/create', 'FairController@create')->middleware('role:super_administrador');
-Route::get('/fair/changeName/{$fair_ori}/{$fair_new}', 'FairController@changeName')->middleware('role:super_administrador');
+Route::get('/fair/changeName/{fair_ori}/{fair_new}', 'FairController@changeName')->middleware('role:super_administrador');
 Route::get('/fair/list_all', 'FairController@list_all')->middleware('role:super_administrador');
 Route::get('/fair/to_list', 'FairController@to_list');
 Route::get('/fair/find/{id?}', 'FairController@find');
