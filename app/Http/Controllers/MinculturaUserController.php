82531f8fe187bbc4611a24147afc2790c1a3a283
David Camacho <davithc01@gmail.com>
2022-11-07 23:47:29 -0500

    registrar evento usuario

--- app/Http/Controllers/MinculturaUserController.php
+++ app/Http/Controllers/MinculturaUserController.php
@@ -15,6 +15,12 @@ class MinculturaUserController extends Controller
 
         //query mincultura user data
         $mincultura = MinculturaUser::where('user_id',$user->id)->first();
+        $minculturaUser->documento_tipo = $data['documento_tipo'];
+        $minculturaUser->documento_numero = $data['documento_numero'];
+        $minculturaUser->correo_electronico_adicional = $data['correo_electronico_adicional'];
+        $minculturaUser->save();
+        
+
         $audience_user = Audience::with('agenda.category')
         ->with('user.user_roles_fair')
         ->where('user_id',$user->id)->get();
@@ -56,4 +62,89 @@ class MinculturaUserController extends Controller
         ];
 
     }
+
+    public function register(Request $request){
+
+        $user = auth()->guard('api')->user();
+
+        //query mincultura user data
+        $minculturaUpdate = false;
+        $mincultura = MinculturaUser::where('user_id',$user->id)->first();
+        if($mincultura) {
+            $minculturaUser->documento_tipo = $data['documento_tipo'];
+            $minculturaUser->documento_numero = $data['documento_numero'];
+            $minculturaUser->correo_electronico_adicional = $data['correo_electronico_adicional'];
+            $minculturaUser->save();
+            $minculturaUpdate = true;
+        }
+
+        //query register in agenda
+        $audience_user = null;
+        $meetings = [];
+        $newAudience = false;
+        $audience = null;
+        $agenda_id = $request['agenda_id'];        
+        if($agenda_id) {
+            $audience_user = Audience::where('user_id',$user->id)->get();
+            if(!$audience_user) {
+                $newAudience = true;
+            }
+            else {
+                $newAudience = true;
+                foreach ($audience_user as $aud) {
+                    if($aud->agenda_id == $agenda_id) {
+                        $audience = $aud;
+                        $newAudience = false;
+                    }
+                }
+            }
+
+            if($newAudience) {
+                Audience::where('user_id',$user->id)->delete();
+                $audience = new Audience();
+                $audience->agenda_id = $agenda_id;
+                $audience->email = $audi['email'];
+                $audience->user_id = $audi['email'];
+                $audience->check = 1;
+                $newAudience = true;                
+                $audience->save();
+            }            
+        }
+        
+        //return data with state available meetings
+        return [
+            'success' => 201,
+            'minculturaUpdate' => $minculturaUpdate,
+            'audience' => $audience,
+            'newAudience' => $newAudience
+        ];
+    }
+
+    public function agendaAvailability(Request $request){
+
+        $agenda_id = $request['agenda_id'];
+        
+        $agenda = Agendas::select('id','title','description', 'description_large','duration_time','start_at','timezone','audience_config','resources','category_id','price');
+        $agenda = $agenda->with('audience.user.user_roles_fair', 'invited_speakers.speaker.user', 'category' );
+        $agenda = $agenda->where([['fair_id',$fair_id],['agenda_id',$agenda_id]])->first();
+
+        if($agenda->audience_config == "5"||$agenda->category->name == 'Taller') {
+            $count = 0;
+            $guest = 25;
+            forEach($agenda->audience as $audience) {
+                if($audience->user->user_roles_fair) {
+                    forEach($audience->user->user_roles_fair as $rol) {
+                        if($rol->pivot->role_id == 4) {
+                            $count++;
+                        }
+                    }
+                }
+            }
+            
+            return $count >= $guest;
+        }
+        
+        return true;
+    } 
+
 }
