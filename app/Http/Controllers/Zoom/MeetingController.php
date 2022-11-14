3e457f4c7cb2c42d1642ee12f3f7e9ea5f802ac5
David Camacho <davithc01@gmail.com>
2022-11-13 21:29:47 -0500

    Update MeetingController.php

--- app/Http/Controllers/Zoom/MeetingController.php
+++ app/Http/Controllers/Zoom/MeetingController.php
@@ -267,7 +267,7 @@ class MeetingController extends Controller
         $data = $validator->validated();
 
         //salatar modificacion zoom
-        $saltar = true;
+        $saltar = false;
 
         if($saltar == false) {
 
