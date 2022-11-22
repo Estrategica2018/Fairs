36a9d818333ec194df3446d39dd01f6a18cb70b1
David Camacho <davithc01@gmail.com>
2022-11-21 23:16:36 -0500

    Update zoomViewer.blade.php

--- resources/views/zoom/zoomViewer.blade.php
+++ resources/views/zoom/zoomViewer.blade.php
@@ -2,8 +2,8 @@
   <head>
     <title>Zoom WebSDK</title>
     <meta charset="utf-8" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.0/css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/1.9.0/css/react-select.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.3.5/css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.3.5/css/react-select.css" />
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="origin-trial" content="">
@@ -42,11 +42,11 @@
     </style>
   </head>
   <body>
-    <script src="https://source.zoom.us/1.9.0/lib/vendor/react.min.js"></script>
-    <script src="https://source.zoom.us/1.9.0/lib/vendor/react-dom.min.js"></script>
-    <script src="https://source.zoom.us/1.9.0/lib/vendor/redux.min.js"></script>
-    <script src="https://source.zoom.us/1.9.0/lib/vendor/redux-thunk.min.js"></script>
-    <script src="https://source.zoom.us/1.9.0/lib/vendor/lodash.min.js"></script>
+    <script src="https://source.zoom.us/2.3.5/lib/vendor/react.min.js"></script>
+    <script src="https://source.zoom.us/2.3.5/lib/vendor/react-dom.min.js"></script>
+    <script src="https://source.zoom.us/2.3.5/lib/vendor/redux.min.js"></script>
+    <script src="https://source.zoom.us/2.3.5/lib/vendor/redux-thunk.min.js"></script>
+    <script src="https://source.zoom.us/2.3.5/lib/vendor/lodash.min.js"></script>
     <script src="https://source.zoom.us/zoom-meeting-1.9.0.min.js"></script>
     <script src="/zoom-client-js/tool.js"></script>
     <script src="/zoom-client-js/vconsole.min.js"></script>
