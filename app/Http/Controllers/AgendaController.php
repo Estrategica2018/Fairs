516fab0e559640042a12e43a86a63ed5687abcc6
David Camacho <davithc01@gmail.com>
2022-11-22 22:04:17 -0500

    Update AgendaController.php

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -384,8 +384,8 @@ class AgendaController extends Controller
         ->get();
 
         foreach($query as $meeting) {
-            if($meeting->category->name != 'Taller_M' && $meeting->category->name != 'Taller_T'  && $meeting->category->name != 'PRUEBAS1') {
-                $start = date('Y-m-d H:i', strtotime('-15 min',$meeting->start_at));
+            if($meeting->category->name != 'Taller_M' && $meeting->category->name != 'Taller_T'  && $meeting->category->name != 'PRUEBAS') {
+                $start = date('Y-m-d H:i', strtotime('-5 min',$meeting->start_at));
                 $end = date('Y-m-d H:i', strtotime('+'.$meeting->duration_time.' min',$meeting->start_at));
                 $date = date('Y-m-d H:i');
                 if (($date >= $start  && $date <= $end)) {                
