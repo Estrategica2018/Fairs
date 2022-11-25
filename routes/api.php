1f8741cf070530f9ea26452cd3ab2291f5914eee
David Camacho <davithc01@gmail.com>
2022-11-25 18:52:40 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -85,7 +85,8 @@ Route::get('/stand/to_list/{pavilion_id?}', 'StandController@to_list');
 //
 Route::post('/user/create', 'UserController@create');
 Route::post('/user/update', 'UserController@update');
-Route::get('/user/delete/{email}/{type}', 'UserController@delete');//->middleware('role:super_administrador');
+//Route::get('/user/delete/{email}/{type}', 'UserController@delete');//->middleware('role:super_administrador');
+Route::get('/user/delete/{email}/{type}/{agenda_id?}', 'UserController@delete');//->middleware('role:super_administrador');
 Route::get('/user/to_list', 'UserController@to_list')->middleware('role:super_administrador');
 Route::get('/user/activate/account/{user_id}', 'UserController@activate_account');
 Route::get('/user/find/{email}', 'UserController@find');
